811ca30e60c1c4af014f75d16da838e25b817f73
venkat <venkat@gmail.com>
2023-11-21 13:03:10 +0530

    intial commit

---
+++ src/main/java/com/getinfy/SpringApiDockerApplication.java
@@ -0,0 +1,13 @@
+package com.getinfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getinfy/SpringApiDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getinfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
